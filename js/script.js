26e63a3abb80a649c1390a798eb706ceee1fe49d
devysonsilva <devysonssilva@gmail.com>
2025-08-06 10:53:58 -0300

    update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ const mainNav = document.getElementById('main-nav');
 navToggle.addEventListener('click', () => {
   mainNav.classList.toggle('open');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll("[data-animate]");
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const el = entry.target;
+        const animation = el.getAttribute("data-animate");
+        el.classList.add(`animate-${animation}`, "visible");
+        observer.unobserve(el);
+      }
+    });
+  }, { threshold: 0.2 }); // 20% visível para acionar
+
+  elements.forEach(el => observer.observe(el));
+});
